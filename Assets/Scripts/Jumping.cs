74ac4cae8c7bccc147060a26d4e6224d58204fae
Ruslan Khakimov <izuick@gmail.com>
2019-06-02 01:33:32 +0500

    Check for ground

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -4,13 +4,14 @@ public class Jumping : MonoBehaviour
 {
 	public float Force;
 	public Rigidbody2D Body;
+	public GroundCheck GroundCheck;
 
 	private bool isJumpPressed;
 
 	private void Update()
 	{
 		isJumpPressed = Input.GetButton("Jump");
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown("Jump") && GroundCheck.Grounded)
 			Body.AddForce(new Vector2(0f, Force), ForceMode2D.Impulse);
 	}
 
@@ -20,7 +21,7 @@ public class Jumping : MonoBehaviour
 		{
 			Gizmos.color = Color.red;
 			Gizmos.DrawSphere(
-				Camera.main.ScreenToWorldPoint(Vector3.one * 100f), 
+				Camera.main.ScreenToWorldPoint(Vector3.one * 100f),
 				1f
 			);
 		}
