2c5673b20f3f8a51e06aac18818fea29d83ee60d
Ruslan Khakimov <izuick@gmail.com>
2019-06-02 01:03:44 +0500

    GroundCheck

---
+++ Assets/GroundCheck.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour
+{
+	public bool Grounded = false;
+	public float GroundCheckRadious = 0.1f;
+	public LayerMask GroundLayer;
+
+	protected virtual void FixedUpdate()
+	{
+		Grounded = Physics2D.OverlapCircle(transform.position, GroundCheckRadious, GroundLayer);
+	}
+}
