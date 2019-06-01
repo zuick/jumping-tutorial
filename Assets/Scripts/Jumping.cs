01e6859b4b41ae38025c88f81a96a9fa4599a402
Ruslan Khakimov <izuick@gmail.com>
2019-06-02 01:25:43 +0500

    Draw Gizmoz

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -5,10 +5,24 @@ public class Jumping : MonoBehaviour
 	public float Force;
 	public Rigidbody2D Body;
 
+	private bool isJumpPressed;
+
 	private void Update()
 	{
-		var jump = Input.GetButtonDown("Jump");
-		if (jump)
+		isJumpPressed = Input.GetButton("Jump");
+		if (Input.GetButtonDown("Jump"))
 			Body.AddForce(new Vector2(0f, Force), ForceMode2D.Impulse);
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (isJumpPressed)
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(
+				Camera.main.ScreenToWorldPoint(Vector3.one * 100f), 
+				1f
+			);
+		}
 	}
 }
