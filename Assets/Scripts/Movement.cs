c5c1f040f621faf30b09a45fda3a19cc49f991b5
Ruslan Khakimov <izuick@gmail.com>
2019-06-02 00:22:41 +0500

    Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float Speed;
+	public Rigidbody2D Body;
+
+	private void Update()
+	{
+		var moveX = Input.GetAxis("Horizontal");
+        Debug.Log(moveX);
+
+		if (Mathf.Abs(moveX) > 0.1f)
+			transform.rotation = Quaternion.Euler(0f, moveX < 0 ? -180f : 0f, 0f);
+
+		Body.velocity = new Vector2(moveX * Speed, Body.velocity.y);
+
+	}
+}
